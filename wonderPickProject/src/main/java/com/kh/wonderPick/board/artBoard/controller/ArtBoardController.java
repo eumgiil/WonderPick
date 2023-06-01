1bd443fc2d39298aa05a387ee3547743e20cf52e
pgaey <tndjqspring@gmail.com>
2023-06-01 16:17:18 +0900

    index.jsp 한 줄 추가 , artBoard 전반 -예찬-
    index.jsp 한 줄 추가 , artBoard 전반 -예찬-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/controller/ArtBoardController.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/controller/ArtBoardController.java
@@ -1,8 +1,23 @@
 package com.kh.wonderPick.board.artBoard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.wonderPick.board.artBoard.model.service.ArtBoardService;
 
 @Controller
 public class ArtBoardController {
 
+	@Autowired
+	private ArtBoardService artService;
+	
+	@RequestMapping("artList.bo")
+	public ModelAndView selectArtList(ModelAndView mv /* int currentPage*/ ) {
+		
+		
+		return mv;
+	} 
+	
 }
