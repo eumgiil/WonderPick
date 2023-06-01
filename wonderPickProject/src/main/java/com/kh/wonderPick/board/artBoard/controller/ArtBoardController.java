300c7075f098a94523fb902a951f94775a9579b5
pgaey <tndjqspring@gmail.com>
2023-06-01 11:49:54 +0900

    ArtBoard 추가  -예찬-

---
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/controller/ArtBoardController.java
@@ -0,0 +1,5 @@
+package com.kh.wonderPick.board.artBoard.controller;
+
+public class ArtBoardController {
+
+}
---
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/service/ArtBoardService.java
@@ -0,0 +1,5 @@
+package com.kh.wonderPick.board.artBoard.model.service;
+
+public interface ArtBoardService {
+
+}
