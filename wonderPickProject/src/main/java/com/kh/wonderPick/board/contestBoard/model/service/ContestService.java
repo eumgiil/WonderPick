fb7386ceff6b448d88d9c8425aadd334778f4879
gi_ill <eomxx94@gmail.com>
2023-06-01 17:27:22 +0900

    -기일-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/service/ContestService.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/service/ContestService.java
@@ -1,11 +1,59 @@
 package com.kh.wonderPick.board.contestBoard.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.wonderPick.board.contestBoard.model.vo.Emoticon;
+
 public interface ContestService {
 
-	void selectMain();
+
 	
 	// 공모전 메인페이지 리스트 조회 
-	// 여기는 역대우승 이모티콘만 불러오면 됨(10개) + 댓글기능  
+	// 이모티콘 사진, 제목, 투표수, 작가사진, 작가이름,
+	// 여기는 역대우승 이모티콘만 불러오면 됨(10개) + 댓글기능
+	ArrayList<Emoticon> selectMain();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	// 페이징처리 
--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/service/ContestServiceImpl.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/service/ContestServiceImpl.java
@@ -1,8 +1,26 @@
 package com.kh.wonderPick.board.contestBoard.model.service;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.wonderPick.board.contestBoard.model.dao.ContestDao;
+import com.kh.wonderPick.board.contestBoard.model.vo.Emoticon;
 
 @Service
 public class ContestServiceImpl implements ContestService {
+	
+	@Autowired
+	private ContestDao contestDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public ArrayList<Emoticon> selectMain() {
+		return contestDao.selectMain(sqlSession);
+	}
 
 }
