f99dd561afc169a759fcf347b45dfbc98e452899
pgaey <tndjqspring@gmail.com>
2023-06-01 10:34:17 +0900

    띄어쓰기 -예찬-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/model/vo/Board.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/model/vo/Board.java
@@ -9,6 +9,6 @@ public class Board {
 	private String createDate; // CREATE_DATE	DATE
 	private String status; // STATUS	VARCHAR2(1 BYTE)
 	private int boardCount; //  BOARD_COUNT	NUMBER
-	private String boardType; // BOARD_TYPE	VARCHAR2(2 BYTE)
+	private String boardType; // BOARD_TYPE	VARCHAR2(2 BYTE) 
 	
 }
