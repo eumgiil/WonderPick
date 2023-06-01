9879e2238a9aaf5a649f4c4e039a9249eef931df
pgaey <tndjqspring@gmail.com>
2023-06-01 10:31:06 +0900

    Board Vo 만들었습니다 - 예찬 -

---
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/model/vo/Board.java
@@ -0,0 +1,14 @@
+package com.kh.wonderPick.board.model.vo;
+
+public class Board {
+
+	private int boardNo; // BOARD_NO	NUMBER
+	private int memberNo; // MEMBER_NO	NUMBER
+	private String boardTitle; // BOARD_TITLE	VARCHAR2(100 BYTE)
+	private String board_content; //BOARD_CONTENT	VARCHAR2(3000 BYTE)
+	private String createDate; // CREATE_DATE	DATE
+	private String status; // STATUS	VARCHAR2(1 BYTE)
+	private int boardCount; //  BOARD_COUNT	NUMBER
+	private String boardType; // BOARD_TYPE	VARCHAR2(2 BYTE)
+	
+}
