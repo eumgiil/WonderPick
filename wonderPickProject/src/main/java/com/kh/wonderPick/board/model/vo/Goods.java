232e1ae09b26d27e2f0a477e1c0d4401110813d2
gyubin0714 <gyubin99@gmail.com>
2023-06-01 12:08:17 +0900

    index 파일 업데이트

---
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/model/vo/Goods.java
@@ -0,0 +1,19 @@
+package com.kh.wonderPick.board.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Goods {
+	
+	private int boardNo; //BOARD_NO	NUMBER
+	private int price; //PRICE	NUMBER
+	private int draft; //DRAFT	NUMBER
+	private int modifyCount; //MODIFY_COUNT	NUMBER
+	private String workday; //WORKDAY	VARCHAR2(100 BYTE)
+	private int addDraft; //ADD_DRAFT	NUMBER
+	private int addModify; //ADD_MODIFY	NUMBER
+	private int amount; //AMOUNT	NUMBER
+	private String goodsCategory; //GOODS_CATEGORY	VARCHAR2(15 BYTE)
+	private String addInfo; //ADD_INFO	VARCHAR2(1000 BYTE)
+
+}
