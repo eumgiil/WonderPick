24140d492321f37388db6b4d3917f8251bdeb5d0
gi_ill <eomxx94@gmail.com>
2023-06-02 12:49:58 +0900

    -기일-
    밥먹고 합시자

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/vo/Contest.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/model/vo/Contest.java
@@ -1,5 +1,21 @@
 package com.kh.wonderPick.board.contestBoard.model.vo;
 
-public class Contest {
+import com.kh.wonderPick.board.boardCommon.model.vo.Board;
+
+import lombok.Data;
+
+@Data
+public class Contest extends Board{
+	
+	private int emoticonNo; 	// EMOTICON_NO	NUMBER
+	private int boardNo; 		// BOARD_NO	NUMBER
+	private int img_no; 		// IMG_NO	NUMBER
+	private int price; 			// PRICE	NUMBER
+	private int priceStatus; 	// PRICE_STATUS	NUMBER
+	
+	//투표 추가 
+	private int voteStatus;
+	private int voteCount;
+	
 
 }
