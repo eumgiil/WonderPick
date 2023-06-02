23b8f46822347a90c88969406cdb3a6a9ad9de28
pgaey <tndjqspring@gmail.com>
2023-06-02 09:17:42 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/eumgiil/WonderPick""
    This reverts commit 6eafe336852257c50a63cb5874c40922494417c7.

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/vo/ArtBoard.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/vo/ArtBoard.java
@@ -6,14 +6,12 @@ import lombok.Data;
 
 @Data
 public class ArtBoard extends Board{
-	
+
 	private String fileType; // FILE_TYPE	VARCHAR2(5 BYTE)
 	private String dpi; // DPI	VARCHAR2(20 BYTE)
 	private String defaultSize; // DEFAULT_SIZE	VARCHAR2(50 BYTE)
 	private int modifyCount; // MODIFY_COUNT	NUMBER
 	private String workday; // WORKDAY	VARCHAR2(100 BYTE)
 	private String category; // CATEGORY	VARCHAR2(2 BYTE)
-	
-	
 	
 }
