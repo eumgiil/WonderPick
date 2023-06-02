6c2f5c23a25610b975177dcb452367bfb2ea6e2c
gi_ill <eomxx94@gmail.com>
2023-06-02 16:34:09 +0900

    -gi_ill-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
@@ -1,10 +1,14 @@
 package com.kh.wonderPick.board.contestBoard.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.wonderPick.board.contestBoard.model.service.ContestService;
+import com.kh.wonderPick.board.contestBoard.model.vo.Contest;
 
 @Controller
 public class ContestController {
@@ -13,9 +17,10 @@ public class ContestController {
 	private ContestService contestService;
 	
 	@RequestMapping("selectTop10.ct")
-	public String selectTop10() {
+	public String selectTop10(Model model) {
 		
-		contestService.selectTop10();
+		ArrayList<Contest> list = contestService.selectTop10();
+		
 		return "board/contestBoard/contestMain";
 		
 		
