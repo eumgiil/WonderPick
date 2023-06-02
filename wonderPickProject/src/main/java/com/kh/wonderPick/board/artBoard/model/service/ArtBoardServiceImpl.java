6eafe336852257c50a63cb5874c40922494417c7
pgaey <tndjqspring@gmail.com>
2023-06-02 09:17:05 +0900

    Revert "Merge branch 'main' of https://github.com/eumgiil/WonderPick"
    This reverts commit 0621eb3c8525d9cb805d3b8f91c8edb55ed148fd, reversing
    changes made to b11b11dda175fe7d6af91cd55e529faf3348c527.

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/service/ArtBoardServiceImpl.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/service/ArtBoardServiceImpl.java
@@ -1,8 +1,27 @@
 package com.kh.wonderPick.board.artBoard.model.service;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.wonderPick.board.artBoard.model.dao.ArtBoardDao;
+import com.kh.wonderPick.board.artBoard.model.vo.ArtBoard;
 
 @Service
 public class ArtBoardServiceImpl implements ArtBoardService {
+
+	@Autowired
+	private ArtBoardDao artDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	@Override
+	public ArrayList<ArtBoard> selectArtList() {
+		return artDao.selectArtList(sqlSession);
+	}
 
 }
