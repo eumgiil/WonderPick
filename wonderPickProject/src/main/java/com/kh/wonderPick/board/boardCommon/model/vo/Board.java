704de20efe15c3d22468c7bad1a9868083c9f151
gi_ill <eomxx94@gmail.com>
2023-06-05 11:32:35 +0900

    board vo 오타수정 -기일-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/boardCommon/model/vo/Board.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/boardCommon/model/vo/Board.java
@@ -9,7 +9,7 @@ public class Board {
 	private int boardNo; // BOARD_NO	NUMBER
 	private int memberNo; // MEMBER_NO	NUMBER
 	private String boardTitle; // BOARD_TITLE	VARCHAR2(100 BYTE)
-	private String board_content; //BOARD_CONTENT	VARCHAR2(3000 BYTE)
+	private String boardContent; //BOARD_CONTENT	VARCHAR2(3000 BYTE)
 	private String createDate; // CREATE_DATE	DATE
 	private String status; // STATUS	VARCHAR2(1 BYTE)
 	private int boardCount; //  BOARD_COUNT	NUMBER
