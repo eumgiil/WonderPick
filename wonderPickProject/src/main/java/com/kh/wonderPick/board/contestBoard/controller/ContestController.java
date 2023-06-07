b74c63911cd2975ca1a17e230398e4976cf88815
gi_ill <eomxx94@gmail.com>
2023-06-07 17:43:41 +0900

    -gi_ill-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
@@ -1,8 +1,8 @@
 package com.kh.wonderPick.board.contestBoard.controller;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.servlet.http.HttpSession;
@@ -39,52 +39,137 @@ public class ContestController {
 	}
 	
 	@RequestMapping("insertContest.ct")
-	public String insertContest(Board b,
-								BoardImage bi,
-								Contest c,
+	public String insertContest(Board board,
+//								BoardImage boardImage,
+								MultipartFile thumbnailUpFile,
 								MultipartFile[] upFile,
+								Contest contest,
 								Model model,
 								HttpSession session) {
 		
-		System.out.println(b);
-		System.out.println(upFile.length);
+		//System.out.println(b);
+		//System.out.println(upFile[0]);
+		//System.out.println(c);
+		System.out.println("썸네일" + thumbnailUpFile);
+
+		ArrayList list = new ArrayList();
 		
-	
+		String originName = thumbnailUpFile.getOriginalFilename();
+		
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+		
+		String ext = originName.substring(originName.lastIndexOf("."));
+		
+		int randomNumber = (int)(Math.random() * 9000 + 1000);
+		
+		String changeName = currentTime + randomNumber + ext;
+		
+		String savePath = session.getServletContext().getRealPath("/resources/boardUpfiles/emoticonFiles/");
+		
+		//multipartFile.transferTo(new File(savePath + changeName));
+		
+		BoardImage boardImage =  new BoardImage();
+		boardImage.setOriginName(originName);
+		boardImage.setModifyName(board.getBoardTitle());
+		boardImage.setFileLevel(1);
+		boardImage.setFilePath("/resources/boardUpfiles/emoticonFiles/" + changeName);
+		
+		list.add(boardImage);
+		
+		
+		
+		
 		
 		for(MultipartFile multipartFile : upFile) {
 			
-			String originName = multipartFile.getOriginalFilename();
-			
-			String currentTime = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
-			
-			int randomNumber = (int)(Math.random() * 90000 + 10000);
-			
-			String ext = originName.substring(originName.lastIndexOf("."));
-			
-			String changeName = currentTime + randomNumber + ext;
-			
-			
-			String savePath = session.getServletContext().getRealPath("/resources/boardUpfiles/emoticonFiles/");
-			
-			try {
-				multipartFile.transferTo(new File(savePath + changeName));
-			} catch (IllegalStateException | IOException e) {
-				e.printStackTrace();
+			if(multipartFile.getSize() != 0) {
+				
+				
+				
+				String originName2 = multipartFile.getOriginalFilename();
+				
+				System.out.println("나머지들 :" + originName2);
+				
+				String currentTime2 = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+				
+				String ext2 = originName2.substring(originName2.lastIndexOf("."));
+				
+				int randomNumber2 = (int)(Math.random() * 9000 + 1000);
+				
+				String changeName2 = currentTime2 + randomNumber2 + ext2;
+				
+				String savePath2 = session.getServletContext().getRealPath("/resources/boardUpfiles/emoticonFiles/");
+				
+				try {
+					multipartFile.transferTo(new File(savePath + changeName));
+				} catch (IllegalStateException | IOException e) {
+					e.printStackTrace();
+				}
+				
+				boardImage =  new BoardImage();
+				boardImage.setOriginName(originName2);
+				boardImage.setModifyName(board.getBoardTitle());
+				boardImage.setFileLevel(2);
+				boardImage.setFilePath("/resources/boardUpfiles/emoticonFiles/" + changeName2);
+				
+				list.add(boardImage);
 			}
 			
-			bi.setOriginName(multipartFile.getOriginalFilename());
-			bi.setModifyName("/resources/boardUpfiles/emoticonFiles/");
-			
+		}
 		
-	}
+		//System.out.println(list);
 		
 		
+//		for(int i = 0; i < list.size(); i++) {
+//			
+//			String originName = list.get(i)
+//			
+//			String currentTime = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+//			System.out.println("1");
+//			
+//			String ext = originName.substring(originName.lastIndexOf("."));
+//			
+//			System.out.println("2");
+//			
+//			int randomNumber = (int)(Math.random() * 90000 + 10000);
+//			
+//			String changeName = currentTime + randomNumber + ext;
+//			System.out.println("3");
+//			
+//			String savePath = session.getServletContext().getRealPath("/resources/boardUpfiles/emoticonFiles/");
+//			
+//			
+//				//multipartFile.transferTo(new File(savePath + changeName));
+//				
+//			
+//			
+//			boardImage.setOriginName(originName);
+//			boardImage.setModifyName(board.getBoardTitle());
+//			boardImage.setFilePath("/resources/boardUpfiles/emoticonFiles/" + changeName);
+//			
+//			if(i == 0) {
+//				boardImage.setFileLevel(1);
+//			}else {
+//				boardImage.setFileLevel(2);
+//			}
+//			
+//			
+////			list.add(boardImage);
+//		
+//		}
 		
 		
-		if(contestService.insertContest(b, bi, c) > 0) {
+		
+
+	
+		//System.out.println("number 1 :" +  b);
+		//System.out.println("22222 : " + bi);
+		//System.out.println("33 :" + c);
+		
+		
+		if(contestService.insertContest(board, list, contest) > 0) {
 			session.setAttribute("alertMsg", "공모전 등록 성공!!" );
 			return "board/contestBoard/contestMain";
-			
 			
 		}else {
 			model.addAttribute("errorMsg", "게시글 작성에 실패하였습니다.");
@@ -97,5 +182,6 @@ public class ContestController {
 		
 
 	}
+
 
 }
