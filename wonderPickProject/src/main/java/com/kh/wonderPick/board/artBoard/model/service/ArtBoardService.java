3e276ed390525fe9dcade58e8e5e244cb2162167
pgaey <tndjqspring@gmail.com>
2023-06-08 14:28:28 +0900

    -예찬-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/service/ArtBoardService.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/service/ArtBoardService.java
@@ -3,15 +3,13 @@ package com.kh.wonderPick.board.artBoard.model.service;
 import java.util.ArrayList;
 
 import com.kh.wonderPick.board.artBoard.model.vo.ArtBoard;
-import com.kh.wonderPick.board.artBoard.model.vo.DetailOp;
-import com.kh.wonderPick.board.artBoard.model.vo.MainOption;
+import com.kh.wonderPick.board.artBoard.model.vo.Option;
 import com.kh.wonderPick.board.boardCommon.model.vo.Board;
-import com.kh.wonderPick.board.boardCommon.model.vo.BoardImage;
 
 public interface ArtBoardService {
 
 	ArrayList<ArtBoard> selectArtList();
 	
-	int insertArtBoard(Board board, ArtBoard artBoard, /*BoardImage boardImg,*/ ArrayList<DetailOp> list);
+	int insertArtBoard(Board board, ArtBoard artBoard, /*BoardImage boardImg,*/ ArrayList<Option> list);
 	
 }
