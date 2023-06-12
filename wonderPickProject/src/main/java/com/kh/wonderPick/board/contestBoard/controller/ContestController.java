4176abc97c8969af72249b38e6cf570b8538ac7a
gi_ill <eomxx94@gmail.com>
2023-06-12 09:24:35 +0900

    -기일-

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/contestBoard/controller/ContestController.java
@@ -146,7 +146,7 @@ public class ContestController {
 		
 		model.addAttribute("list", contestService.selectVotePage());
 		
-		System.out.println("??: " + contestService.selectVotePage());
+		//System.out.println("??: " + contestService.selectVotePage());
 		
 		return "board/contestBoard/contestVote";
 	}
