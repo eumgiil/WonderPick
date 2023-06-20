38475fae3fa54a45ef99851773711cd2ed772dd7
pgaey <tndjqspring@gmail.com>
2023-06-20 15:39:48 +0900

    페이징 처리 예찬

---
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/vo/SearchArt.java
@@ -0,0 +1,12 @@
+package com.kh.wonderPick.board.artBoard.model.vo;
+
+import lombok.Data;
+
+@Data
+public class SearchArt {
+	
+	private String search;
+	private String keyword;
+	private String category;
+	
+}
