ba601a70d75b5a0bda0d717fd69d925b187fb57a
pgaey <tndjqspring@gmail.com>
2023-06-23 10:23:56 +0900

    예찬

--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/controller/ArtBoardController.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/controller/ArtBoardController.java
@@ -1,6 +1,12 @@
 package com.kh.wonderPick.board.artBoard.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -26,7 +33,6 @@ import com.kh.wonderPick.board.boardCommon.model.vo.Board;
 import com.kh.wonderPick.board.boardCommon.model.vo.BoardImage;
 import com.kh.wonderPick.board.reply.model.service.ReplyService;
 import com.kh.wonderPick.board.review.model.service.ReviewService;
-import com.kh.wonderPick.board.review.model.vo.Review;
 import com.kh.wonderPick.common.model.vo.PageInfo;
 import com.kh.wonderPick.common.template.Pagination;
 import com.kh.wonderPick.member.model.vo.Member;
@@ -50,21 +56,12 @@ public class ArtBoardController {
 		System.out.println(searchArt);
 		PageInfo pi;
 		
-//		if(null != searchArt.getSearch() && searchArt.getSearch() != "") {
-//			pi = Pagination.getPageInfo(artService.selectSearchListCount(searchArt), currentPage, 12, 10);
-//			mv.addObject("pi", pi)
-//			  .addObject("list", artService.selectSearchList(pi, searchArt))
-//			  .addObject("searchArt", searchArt)
-//			  .addObject("category", category)
-//			  .setViewName("board/artBoard/artListView");
-//		} else {
-			pi = Pagination.getPageInfo(artService.selectArtListCount(searchArt), currentPage, 12, 10);
-			mv.addObject("pi", pi)
-			  .addObject("list", artService.selectArtList(pi, searchArt))
-			  .addObject("searchArt", searchArt)
-//			  .addObject("category", category)
-			  .setViewName("board/artBoard/artListView");
-//		}
+		pi = Pagination.getPageInfo(artService.selectArtListCount(searchArt), currentPage, 12, 10);
+		mv.addObject("pi", pi)
+		  .addObject("list", artService.selectArtList(pi, searchArt))
+		  .addObject("searchArt", searchArt)
+		  .setViewName("board/artBoard/artListView");
+		
 		System.out.println("pi : " + pi);
 		System.out.println("list : " + artService.selectArtList(pi, searchArt));
 		return mv;
@@ -84,6 +81,13 @@ public class ArtBoardController {
 							   HttpServletRequest request,
 							   Model model) {
 		
+		// 로그인 나오면 지울 부
+		Member loginUser = new Member();
+		loginUser.setMemberNo(1);
+		session.setAttribute("loginUser", loginUser);
+		// 지울 부분 끝
+		
+		// 옵션 ArrayList에 담기
 		ArrayList<Option> list = new ArrayList(); // VO들이 담긴 객체
 		for(int i = 1; i <= options.length; i++) {
 			Option option = new Option();
@@ -117,11 +121,6 @@ public class ArtBoardController {
 		}
 		board.setBoardContent(total.toString());
 		
-		// 로그인 나오면 지울 부
-		Member loginUser = new Member();
-		loginUser.setMemberNo(1);
-		session.setAttribute("loginUser", loginUser);
-		// 지울 부분 끝
 		
 		board.setMemberNo(((Member)session.getAttribute("loginUser")).getMemberNo());
 		int result = artService.insertArtBoard(board, artBoard, list, files);
@@ -134,18 +133,32 @@ public class ArtBoardController {
 	}
 	
 	@RequestMapping(value="artDetail.bo")
-	public ModelAndView artDetail(ModelAndView mv, int bno) {
-
+	public ModelAndView artDetail(ModelAndView mv, int bno, HttpSession session) {
+		
+		System.out.println(bno);
+		// 로그인 나오면 지울 부
+		Member loginUser = new Member();
+		loginUser.setMemberNo(1);
+		session.setAttribute("loginUser", loginUser);
+		// 지울 부분 끝
 		
 //		reply
 //		re_reply
 		
+		// 로그인 번호 받고 selectArtBoard에 bno랑 mno를 int[]로 보내기
 		
-		ArtBoard artBoard = artService.selectArtBoard(bno);
+		
+		int mno = ((Member)session.getAttribute("loginUser")).getMemberNo();
+		HashMap maps = new HashMap();
+		maps.put("mno", mno);
+		maps.put("bno", bno);
+		
+		
+		ArtBoard artBoard = artService.selectArtBoard(maps);
 		ArrayList<Option> optionList = artService.selectOptionList(bno);
 		ArrayList<BoardImage> boardImage = artService.selectBoardImage(bno);
 		
-		ArrayList<Review> reviewList = reviewService.selectBoardReviewList(bno);
+//		ArrayList<Review> reviewList = reviewService.selectBoardReviewList(bno);
 		
 		
 		
@@ -153,9 +166,9 @@ public class ArtBoardController {
 //		ArrayList<Reply1> replyList = replyService.selectReplyList(bno);
 		
 		mv.addObject("artBoard", artBoard)
-		  .addObject("optionList", optionList)
-		  .addObject("boardImage", boardImage)
-		  .addObject("reviewList", reviewList)
+		  .addObject("optionList", new Gson().toJson(optionList))
+		  .addObject("boardImage", new Gson().toJson(boardImage))
+//		  .addObject("reviewList", reviewList)
 		  .addObject("bno",bno)
 //		  .addObject("replyList", replyList)
 		  .setViewName("board/artBoard/artDetailView");
@@ -172,7 +185,154 @@ public class ArtBoardController {
 //		mv.addObject("pi", pi).addObject("list", artService.selectSearchList(pi, searchArt)).setViewName("board/artBoard/artListView");
 //		return mv;
 //	}
+
+	@RequestMapping("updateForm.at")
+	public ModelAndView updateArtBoard(ModelAndView mv, int boardNo, HttpSession session){
+		// 로그인 나오면 지울 부
+		Member loginUser = new Member();
+		loginUser.setMemberNo(1);
+		session.setAttribute("loginUser", loginUser);
+		// 지울 부분 끝
+		
+		
+		
+		int mno = ((Member)session.getAttribute("loginUser")).getMemberNo();
+		
+		HashMap maps = new HashMap();
+		maps.put("mno", mno);
+		maps.put("bno", boardNo);
+		
+		ArtBoard artBoard = artService.selectArtBoard(maps);
+		artBoard.getBoard().setBoardNo(boardNo);
+		ArrayList<Option> optionList = artService.selectOptionList(boardNo);
+		ArrayList<BoardImage> boardImage = artService.selectBoardImage(boardNo);
+		
+		System.out.println(artBoard);
+		System.out.println(optionList.toString());
+		System.out.println(boardImage);
+		
+		mv.addObject("artBoard", artBoard)
+		  .addObject("optionList", new Gson().toJson(optionList))
+		  .addObject("boardImage", new Gson().toJson(boardImage))
+		  .addObject("bno",boardNo)
+		  .setViewName("board/artBoard/artUpdateView");
+		return mv;
+	}
 	
+	@RequestMapping("updateBoard.at")
+	public String updateBoard(Board board,
+						    ArtBoard artBoard,
+						    String[] options,
+						    MultipartFile[] upFile,
+						    HttpSession session,
+						    HttpServletRequest request,
+						    Model model) {
+		
+		
+		// 로그인 나오면 지울 부
+		Member loginUser = new Member();
+		loginUser.setMemberNo(1);
+		session.setAttribute("loginUser", loginUser);
+		// 지울 부분 끝
+		
+		ArrayList<Integer> optionNos = new ArrayList();
+		for(int i = 0; i < artService.selectOptionList(board.getBoardNo()).size(); i++) {
+			optionNos.add(artService.selectOptionList(board.getBoardNo()).get(i).getOptionNo());
+		}
+		
+		System.out.println(optionNos.toString());
+		
+		// 옵션 ArrayList에 담기
+		ArrayList<Option> list = new ArrayList(); // VO들이 담긴 객체
+		for(int i = 1; i <= options.length; i++) {
+			Option option = new Option();
+			ArrayList<DetailOption> detailList = new ArrayList();
+			option.setMainOp(request.getParameter("option_" + i));
+			for(int j = 0; j < request.getParameterValues("detailOp" + i).length; j++) {
+				DetailOption detailOption = new DetailOption();
+				detailOption.setDetail(request.getParameterValues("detailOp" + i)[j]);
+				detailOption.setPrice(request.getParameterValues("opPrice" + i)[j]);
+				detailList.add(detailOption);
+			}
+			option.setDetailOption(detailList);
+			option.setBoardNo(board.getBoardNo());
+			list.add(option);
+		}
+		System.out.println(board);
+		System.out.println(artBoard);
+		System.out.println(list.toString());
+		
+		
+		
+		/*
+			boardContent를 조회해와서 JsonArray를 toString 으로 문자열 비교를 해서
+			같으면 : 상세설명 수정 안해도 됨
+			다르면 : type별로 비교해서 다른 것만 바꿔주되,
+				   img가 다른 것은....
+			
+		*/
+		
+		
+		
+		
+		
+		// resources 파일 삭제
+		int totalFileSize = 0;
+		for(MultipartFile upfile : upFile) {
+			totalFileSize += upfile.getSize();
+		}
+		System.out.println("totalFileSize : " + totalFileSize);
+		if(totalFileSize != 0) {
+			ArrayList<String> filePathList = new ArrayList();
+			for(int i = 0; i < artService.selectBoardImage(board.getBoardNo()).size(); i++) {
+				filePathList.add(artService.selectBoardImage(board.getBoardNo()).get(i).getFilePath());
+			}
+			for(int i = 0; i < filePathList.size(); i++) {
+				new File(filePathList.get(i)).delete();
+			}
+		}
+		
+		
+		// 파일 이름 바꾸기
+		String savePath = session.getServletContext().getRealPath("/resources/boardUpfiles/artBoardFiles/");
+		String folderPath = "resources/boardUpfiles/artBoardFiles/";
+		ArrayList<BoardImage> files = new BoardController().saveFile(upFile, session, savePath, folderPath);
+		System.out.println("upFile[0].getOriginalFilename() : " + upFile[0].getOriginalFilename());
+		System.out.println("upFile.getSize() : " + upFile[0].getSize());
+		System.out.println("upFile.length : " + upFile.length);
+		System.out.println("files : " + files);
+		// 상세설명 영역
+		JsonArray total  = new JsonParser().parse(board.getBoardContent()).getAsJsonArray();
+		System.out.println("total.toString() : " + total.toString());
+		for(int i = 0; i < total.size(); i++) {
+			if("img".equals(total.get(i).getAsJsonObject().get("type").getAsString())) {
+				JsonObject jobj = total.get(i).getAsJsonObject();
+				
+				System.out.println("jobj : " + jobj);
+				System.out.println("files.get(files.size()-1).getModifyName() : " + files.get(files.size()-1).getModifyName());
+				
+				jobj.keySet().remove("data");
+				jobj.addProperty("data", files.get(files.size()-1).getModifyName());
+				files.get(files.size()-1).setFileLevel(3);
+			}
+		}
+		board.setBoardContent(total.toString());
+		
+		
+		board.setMemberNo(((Member)session.getAttribute("loginUser")).getMemberNo());
+		int result = artService.updateArtBoard(board, artBoard, list, files, optionNos);
+//		if(result > 0) {
+//			model.addAttribute("alertMsg", "업로드 성공");
+//		} else {
+//			model.addAttribute("alertMsg", "업로드 실패");
+//		}
+		return "redirect:artList.bo?category=CI";
+	}
+	
+	@RequestMapping("deleteBoard.at")
+	public String deleteBoard(int boardNo) {
+		return "redirect:artList.bo";
+	}
 	
 	
 	
--- wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/vo/ArtBoardDTO.java
+++ wonderPickProject/src/main/java/com/kh/wonderPick/board/artBoard/model/vo/ArtBoardDTO.java
@@ -13,5 +13,6 @@ public class ArtBoardDTO {
 	private String modifyName;
 	private String memberModifyName;
 	private String category;
+	private int heart;
 	
 }
